5cc199ee9e942a14ee358fba0443e4ba2b1267d2
Marvin vissers <32897103+Booreilandtje@users.noreply.github.com>
2020-04-20 15:45:28 +0200

    Basic links to styling and script

---
+++ head.php
@@ -0,0 +1,48 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <!-- Add the folowing things yourself
+        - Author
+        - Title
+        - Description
+    -->
+
+    <head>
+        <!--Preloading all important things-->
+        <link rel="preload" href="assests/styling/main.css" as="style">
+        <link rel="preload" href="assests/script/main.js" as="script">
+        <link rel="preload" href="assests/script/validation.js" as="script">
+        <link rel="preload" href="assests/bootstrap/css/bootstrap.min.css" as="style">
+        <link rel="preload" href="assests/bootstrap/js/bootstrap.min.js" as="script">
+
+        <!-- all meta tags -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Linking to Bootstrap CSS -->
+        <link rel="stylesheet" href="assests/bootstrap/css/bootstrap.min.css">
+
+        <!-- Linking to Bootstrap JavaScript -->
+        <script src="assests/bootstrap/js/bootstrap.min.js"></script>
+
+        <!-- Linking voor jQuery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+        <!-- Linking to own CSS -->
+        <link rel="stylesheet" href="assests/styling/main.css">
+
+        <!-- Linking to own JavaScript -->
+        <script src="assests/script/validation.js"></script>
+        <script src="assests/script/main.js"></script>
+
+        <!-- Linking to icons -->
+        <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+
+        <!-- Linking to my favicon -->
+        <link rel="icon" href="assets/images/AssistRecruitmentFavicon.ico">
+
+        <!-- Datatables toevoegen -->
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.20/sp-1.0.1/sl-1.3.1/datatables.min.css%22/%3E">
+        <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.20/sp-1.0.1/sl-1.3.1/datatables.min.js%22%3E"></script>
+    </head>
+</html>

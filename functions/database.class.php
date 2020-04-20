f849bc39db862f07d068e1e75d034b53b18f19cc
StickFunFight <36295961+StickFunFight@users.noreply.github.com>
2020-04-20 15:13:58 +0200

    Gegevens

--- functions/database.class.php
+++ functions/database.class.php
@@ -6,9 +6,9 @@
         public $conn;
 
         // De gegevens om de connectie mee te maken
-        private $host = 'localhost';
-        private $db_name = 'assistrecruitment';
-        private $username = 'assist';
+        private $host = 'assist.tk';
+        private $db_name = 'assist';
+        private $username = 'assist_remote';
         private $password = 'KPg$R%Tsd@Y%';
 
         // Connectie maken
@@ -17,11 +17,10 @@
 
             // Kijken of de connectie gemaakt kan worden en anders een foutmelding geven
             try {
-                $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
+                $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name . ';port=3306', $this->username, $this->password);
             } catch (\PDOException $exception) {
                 echo 'Connection error: ' . $exception->getMessage();
             }
-
             // Connectie teruggeven
             return $this->conn;
         }
--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<html>
 <?php
-require ("functions/database.class.php")
+require_once 'functions/database.class.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$statement = $db->prepare('SELECT * FROM question');
+$statement->execute();
+$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+$post_data = json_encode(array('item' => $result));
+
+echo $post_data;
+
 ?>
+</html>
